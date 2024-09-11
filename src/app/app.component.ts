e8c53b8d80fa64927bc8f406970b856568c6e358
anthonyveronica <anthonyveronica2000@gmail.com>
2024-09-11 22:30:44 +0530

    Zoom in + zoom out + api changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,26 +53,9 @@ export class AppComponent implements OnInit{
      this.summeryItems=data
     })
     this.isInitialized = true;
-    this.zoomSubject.next(Math.min(this.zoomLevel, 2));
     this.zoomSubject.subscribe((zoom) => {
       this.zoomLevel = zoom;
-      this.applyZoom();
     });
-    // this.zoomSubject.pipe(
-    //   debounceTime(0.1),  // Adjust the debounce time as needed
-    //   switchMap(zoomLevel => {
-    //     return timer(0, 0.1).pipe(
-    //       switchMap(() => {
-    //         if (this.zoomLevel !== zoomLevel) {
-    //           this.zoomLevel = zoomLevel;
-    //           return interval(0);
-    //         } else {
-    //           return [];
-    //         }
-    //       })
-    //     );
-    //   })
-    // ).subscribe();
   }
 
   get zoomTransform(): string {
@@ -86,21 +69,6 @@ export class AppComponent implements OnInit{
   zoomOut(): void {
     this.zoomSubject.next(Math.max(this.zoomLevel - 0.1, 0.1)); // Minimum zoom level (0.1x)
   }
-
-  applyZoom(): void {
-    if (this.zoomableContent) {
-      // Apply CSS transform to scale the content
-      this.zoomableContent.nativeElement.style.transform = `scale(${this.zoomLevel})`;
-    }
-  }
-
-  // zoomIn(): void {
-  //   this.zoomSubject.next(Math.min(this.zoomLevel + 0.1, 2));
-  // }
-  //
-  // zoomOut(): void {
-  //   this.zoomSubject.next(Math.max(this.zoomLevel - 0.1, 0.1));
-  // }
 
   @HostListener('wheel', ['$event'])
   onMouseWheel(event: WheelEvent): void {
--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 export const  environment ={
     production:true,
     boothUiDetailes:'https://events.indiajoy.in/api/booth-details',
-    boothpostDetailes:'https://events.indiajoy.in/booth-details',
+    boothpostDetailes:'http://127.0.0.1:5000/api/bookings',
     summeryDetailes:'https://events.indiajoy.in/api/categories_summary'
 }
